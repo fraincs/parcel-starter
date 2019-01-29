d129181ab60c1c6a4a7a78e35cb0d6367ba2e828
Francis Thibault <francis@officevibe.com>
2019-01-29 13:34:13 -0500

    setup

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import './sass/custom.scss';
+import './sass/utility.scss';
 
-console.log("hello world");
+const manageLikes = () => {
+    let likes = document.querySelector("[data-likes]");
+    let heart = document.querySelector("[data-heart]");
+    likes.innerHTML = likes.dataset.likes;
+    heart.addEventListener("click", () => {
+        likes.innerHTML = likes.dataset.likes + 1;
+    })
+}
+
+manageLikes();
