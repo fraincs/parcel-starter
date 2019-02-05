9c788ea8d6e39bfb2883e506be8c77bb7e6c64f7
Francis Thibault <francis@officevibe.com>
2019-02-05 13:27:54 -0500

    added some text

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ import './sass/utility.scss';
 
 const manageLikes = () => {
     let likes = document.querySelector("[data-likes]");
-    let heart = document.querySelector("[data-heart]");
     likes.innerHTML = likes.dataset.likes;
-    heart.addEventListener("click", () => {
-        likes.innerHTML = likes.dataset.likes + 1;
-    })
 }
 
 manageLikes();
