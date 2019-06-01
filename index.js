e9c5d14a2bac7d1d5a63632a9e43d00c379dae87
GLOBALIA\flacourse <flacourse@globalia.ca>
2019-06-01 17:39:00 -0400

    - Optimizing images - Structuring Sass files - First draft of HTML dom structure - Testing like +1 feature

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import './sass/custom.scss';
-import './sass/utility.scss';
+import './sass/main.scss';
 
-const manageLikes = () => {
+/*const manageLikes = () => {
     let likes = document.querySelector("[data-likes]");
     likes.innerHTML = likes.dataset.likes;
 }
 
 manageLikes();
+
+const manageComments = () => {
+    let comments = document.querySelector("[data-comments]");
+    comments.innerHTML = comments.dataset.comments;
+}
+
+manageComments();*/
+
+let buttons = document.querySelectorAll(".action");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+        let currentCount = parseInt(this.firstChild.nextSibling.dataset.count);
+        this.firstChild.nextSibling.dataset.count = currentCount + 1;
+    })
+});
